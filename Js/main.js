7edf4f479c415ad8e7adc119ce0600c4dcf24976
Abdelrahman Mohamed <abdelrahmanmo715@gmail.com>
2024-09-20 16:06:25 +0300

    works

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,4 @@
 
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items : 1,
-        dots : true,
-        autoplay : true
-    });
-  });
 
   // Navbar scrolling background
   $(window).on("scroll", function () {
@@ -24,4 +17,30 @@ $(document).ready(function(){
 // Close navbar-collapse when a clicked
 $(".navbar-nav .dropdown-item a").on('click', function () {
     $(".navbar-collapse").removeClass("show");
+});
+
+// Works owlCarousel
+$('.works .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    mouseDrag: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    dots: true,
+    autoplayHoverPause: true,
+    nav: false,
+    navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            dots: true
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
 });
