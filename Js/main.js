73f1bea30f25e574f716f6f268dbf70c0cc8a684
Abdelrahman Mohamed <abdelrahmanmo715@gmail.com>
2024-09-24 00:04:41 +0300

    add baseurl for demo

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,9 @@
+const repoName = "Arcens";
+document.querySelectorAll('a.nav-link').forEach(link => {
+  if (window.location.hostname === "abdelrahmanmo715.github.io") {
+    link.href = `/${repoName}/${link.getAttribute("href")}`;
+  }
+});
 
 
   // Navbar scrolling background
